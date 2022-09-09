26581f58f90fb0e0dc4acf86b8c30754c1a39bf6
yoji-nakamura <yoji_nakamura@precs.co.jp>
2022-09-09 15:44:39 +0900

    🌟 feat: npm show auth

--- src/js/test.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin / env rhino
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-const nameToGreet = core.getInput('test-auth');
-const token = process.env.TEST_AUTH;
-console.log(token);
-console.log(nameToGreet);
